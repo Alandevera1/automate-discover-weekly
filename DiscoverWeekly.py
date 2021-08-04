791d30e30d7bb21ffd0618d5515377a736e28f14
Alandevera1 <alandevera1@vt.edu>
2021-08-04 18:35:08 -0400

    adding python script

---
+++ DiscoverWeekly.py
@@ -0,0 +1,86 @@
+from dotenv import load_dotenv
+import os
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from datetime import datetime
+
+### TODO ###
+# add exception handling
+# create GUI, web app, or website
+# QOL changes
+### TODO ###
+
+load_dotenv()
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv('CLIENT_ID'),
+                                               client_secret=os.getenv('CLIENT_SECRET'),
+                                               redirect_uri=os.getenv('REDIRECT_URI'),
+                                               scope='playlist-modify-private'))
+
+
+BASE_URL = 'https://api.spotify.com/v1/'
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST to get Spotify access token 1
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': os.getenv('CLIENT_ID'), # Spotify integration ID
+    'client_secret': os.getenv('CLIENT_SECRET'), # Spotify integration secret
+    "expires_in": 3600
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+# headers to request anything in the Spotify API 
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+def get_playlist_tracks(playlist_url):
+    # Indices of the URI in the playlist link
+    playlist_id = playlist_url[len(
+        'https://open.spotify.com/playlist/'):len('https://open.spotify.com/playlist/') + 22]
+
+    ''' getting track uris '''
+    playlist = requests.request('GET', BASE_URL + 'playlists/' + playlist_id +
+                            '/tracks', headers=headers)  # Gets the url and requests data
+    playlist = playlist.json()
+    # Takes in only the tracks of a playlist
+    playlist_tracks = playlist.get('items')
+
+    playlist_track_uri = [track.get('track').get(
+        'uri')[14:] for track in playlist_tracks]  # List of uris
+    
+    return playlist_track_uri
+
+def create_playlist_image(img):
+    pass
+
+def img2base64(img_link):
+    import numpy as np
+    import base64
+    import urllib
+    from PIL import Image
+    with open("img_file.jpg", "wb") as f:
+        f.write(urllib.request.urlopen(img_link).read())
+    tmp_img = np.asarray(Image.open("img_file.jpg"))
+    tmp_thumb = tmp_img.resize((250, 250), Image.ANTIALIAS)
+    tmp_thumb.save("thumb_file.jpg")
+    with open("thumb_file.jpg", "rb") as img:
+        thumb_string = base64.b64encode(img.read())
+    base64out = "data:image/jpeg;base64," + str(thumb_string)[2:-1]
+    return(base64out)
+
+def run():
+    name = f'Discover Weekly [{datetime.today().strftime("%#m/%#d/%Y")}]'
+    playlist_id = sp.user_playlist_create(user='alandevera2', name=name, public=False).get('id')
+    sp.playlist_add_items(playlist_id=playlist_id, items=get_playlist_tracks(os.getenv('DISCWEEKLY_URL')))
+    return 'OK'
+    
+''' Main method to run the program '''
+if __name__ == "__main__":
+    run()
